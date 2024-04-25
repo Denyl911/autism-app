409239b1428a3a8b55bea9bed05fec30ef577b14
Denyl911 <imdenyl@gmail.com>
2024-04-24 22:32:44 -0600

    first commit

---
+++ screens/Home.js
@@ -0,0 +1,13 @@
+import { View, Text, Button } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View className="items-center justify-center flex-[1]">
+      <Text className="text-red-500 font-bold">Home Screeen</Text>
+      <Button
+        title="Go to Detalles"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
