b7f3188c5abd67d3057389c702150aee63a156b9
Denyl911 <imdenyl@gmail.com>
2024-05-12 22:53:04 -0600

    face detection v0.1

--- screens/Emotions.jsx
+++ screens/Emotions.jsx
@@ -2,15 +2,31 @@ import { Camera, CameraType } from 'expo-camera';
 import { useState } from 'react';
 import { View, Pressable, Text, StyleSheet, Button, Image } from 'react-native';
 import * as FaceDetector from 'expo-face-detector';
+import { manipulateAsync, FlipType } from 'expo-image-manipulator';
 
 export default function Emotions({ navigation }) {
-  const [xOr, setXOr] = useState(0);
-  const [yOr, setYOr] = useState(0);
-  const [xWid, setXWid] = useState(0);
-  const [xHei, setXHei] = useState(0);
-  const [bord, setBorder] = useState(0);
+  // const [xOr, setXOr] = useState(0);
+  // const [yOr, setYOr] = useState(0);
+  // const [xWid, setXWid] = useState(0);
+  // const [xHei, setXHei] = useState(0);
+  // const [bord, setBorder] = useState(0);
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [cameraRef, setCameraRef] = useState(null);
+  const [emotion, setEmotion] = useState('Escaner de Emociones');
+  const [scanText, setScanText] = useState('ESCANEAR');
+  const [foto, setFoto] = useState(null);
+  const [fotoUri, setFotoUri] = useState(null);
+
+  const trans = {
+    angry: 'Enojado',
+    disgust: 'Disgustado',
+    fear: 'Miedo',
+    happy: 'Felicidad',
+    neutral: 'Neutral',
+    sad: 'Triste',
+    surprise: 'Sorprendido',
+  };
 
   if (!permission) {
     // Camera permissions are still loading
@@ -34,38 +50,85 @@ export default function Emotions({ navigation }) {
       current === CameraType.back ? CameraType.front : CameraType.back
     );
   }
-  function handleFacesDetected(x) {
-    if (x) {
-      x.faces.forEach((el) => {
-        if (el.bounds) {
-          setXHei(el.bounds.size.height);
-          setXWid(el.bounds.size.width);
-          setXOr(el.bounds.origin.x);
-          setYOr(el.bounds.origin.y);
-          setBorder(2);
-          console.log(el.bounds);
-        } else {
-          setBorder(0);
-        }
+  // function handleFacesDetected(x) {
+  //   if (x) {
+  //     x.faces.forEach((el) => {
+  //       if (el.bounds) {
+  //         setXHei(el.bounds.size.height);
+  //         setXWid(el.bounds.size.width);
+  //         setXOr(el.bounds.origin.x);
+  //         setYOr(el.bounds.origin.y);
+  //         setBorder(2);
+  //       } else {
+  //         setBorder(0);
+  //       }
+  //     });
+  //   }
+  // }
+
+  async function scanFace() {
+    if (fotoUri) {
+      setFotoUri(null);
+      setScanText('ESCANEAR');
+      setEmotion('Escaner de Emociones');
+      return;
+    }
+    setEmotion('Escaneando...');
+    setScanText('Volver');
+    try {
+      const img = await cameraRef.takePictureAsync({
+        base64: true,
+        isImageMirror: true,
       });
+      const fliped = await manipulateAsync(img.uri, [
+        { flip: FlipType.Horizontal },
+      ]);
+      setFotoUri(fliped.uri);
+      const res = await fetch(
+        'https://81c1-201-108-4-15.ngrok-free.app/emotions',
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            imagen: img.base64,
+          }),
+        }
+      );
+      const data = await res.json();
+      if (data.emotion != 'No') {
+        setEmotion(
+          data.emotion.replace(/\b\w+\b/g, function (match) {
+            return trans[match] || match;
+          })
+        );
+      } else {
+        setEmotion('No se detectó ninguna');
+      }
+    } catch (e) {
+      console.log(e);
     }
   }
 
-  function facePos() {
-    console.log({
-      height: xHei,
-      with: xWid,
-      position: 'absolute',
-      left: xOr,
-      top: yOr,
-    });
-    return {
-      height: xHei,
-      with: xWid,
-      position: 'absolute',
-      left: xOr,
-      top: yOr,
-    };
+  function ShowImage(props) {
+    if (props.show) {
+      return (
+        <Image
+          style={{
+            borderRadius: 8,
+            borderColor: '#000',
+            borderWidth: 2,
+            height: 472,
+            width: 382,
+          }}
+          source={{
+            uri: fotoUri,
+          }}
+        ></Image>
+      );
+    }
   }
 
   return (
@@ -83,46 +146,60 @@ export default function Emotions({ navigation }) {
             ></Image>
           </Pressable>
         </View>
-        <View style={styles.container}>
+        <View
+          style={{
+            display: fotoUri ? 'none' : 'block',
+            marginHorizontal: 20,
+            borderColor: '#000',
+            borderWidth: 4,
+            borderRadius: 12,
+            height: 480,
+            width: 390,
+          }}
+        >
           <Camera
+            ref={(ref) => setCameraRef(ref)}
             style={styles.camera}
             type={type}
-            onFacesDetected={handleFacesDetected}
+            // onFacesDetected={handleFacesDetected}
             faceDetectorSettings={{
               mode: FaceDetector.FaceDetectorMode.accurate,
               detectLandmarks: FaceDetector.FaceDetectorLandmarks.none,
               runClassifications: FaceDetector.FaceDetectorClassifications.none,
-              minDetectionInterval: 100,
+              minDetectionInterval: 3000,
               tracking: true,
             }}
-          >
-            <View
-              style={{
-                height: xHei,
-                width: xWid,
-                position: 'absolute',
-                left: xOr,
-                bottom: yOr,
-                borderColor: 'red',
-                borderWidth: bord,
-              }}
-            ></View>
-            <View style={styles.buttonContainer}>
-              <Pressable style={styles.button} onPress={toggleCameraType}>
-                <Text style={styles.text}>Flip Camara</Text>
-              </Pressable>
-            </View>
-          </Camera>
+          ></Camera>
         </View>
+        <ShowImage show={fotoUri ? true : false}></ShowImage>
       </View>
-      <View className="felx items-center">
-        <Image
+      <View>
+        <Text className="mt-16 text-3xl font-bold text-center text-sky-900">
+          {emotion}
+        </Text>
+      </View>
+      <View className="flex flex-row justify-between items-center mt-16 mx-5">
+        {/* <Image
           className="w-24 h-24 mt-20"
           source={require('../assets/happy.png')}
-        ></Image>
-        <Text className="mt-3 text-3xl font-bold text-center text-yellow-400">
-          Felicidad
-        </Text>
+        ></Image> */}
+        <Pressable
+          className="py-3 px-1 rounded-full  active:bg-slate-200"
+          onPress={toggleCameraType}
+        >
+          <Text className="text-2xl text-black">Rotar</Text>
+        </Pressable>
+        <Pressable
+          className="px-4 py-3 rounded-full bg-sky-800"
+          onPress={scanFace}
+        >
+          <Text className="text-3xl font-bold text-center text-white">
+            {scanText}
+          </Text>
+        </Pressable>
+        <Pressable className="py-3 px-1 rounded-full  active:bg-slate-200">
+          <Text className="text-2xl text-black">Galeria</Text>
+        </Pressable>
       </View>
     </View>
   );
@@ -134,8 +211,8 @@ const styles = StyleSheet.create({
     borderColor: '#000',
     borderWidth: 4,
     borderRadius: 12,
-    height: 420,
-    width: 340,
+    height: 480,
+    width: 390,
   },
   face: {
     marginHorizontal: 20,
